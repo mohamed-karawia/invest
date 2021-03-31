24afd62a37e91e8189937bedbea6d17ae68b956b
mohamed-karawia <m.karawia11@gmail.com>
2021-03-31 12:10:03 +0200

    basename browser Router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import signinPage from './pages/signin';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={signinPage} exact />
